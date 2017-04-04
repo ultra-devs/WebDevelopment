fe9e17ad760bcac8cb536286d1d71f7f9fc3a312
shanky <shanky@ultra-dev>
2017-04-03 22:35:06 -0500

    adding button trigger

--- src/com/dev/util/db/ApacheDerby.java
+++ src/com/dev/util/db/ApacheDerby.java
@@ -2,20 +2,13 @@ package com.dev.util.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.dbutils.DbUtils;
-
-import com.dev.api.json.beans.Hits;
 
 public class ApacheDerby {
 
